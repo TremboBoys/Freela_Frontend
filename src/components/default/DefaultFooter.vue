50a4d51c6d68ae7c0206a1af7970468a0fb66b3e
dnzlucas <lucasdefenix@gmail.com>
2024-08-31 11:11:25 -0300

    FEAT: Create header and footer components and install Sass. #6

---
+++ src/components/default/DefaultFooter.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+</template>
+<style scoped></style>
---
+++ src/components/default/DefaultHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <header>
+        <div class="header-leftSide">
+            <img src="../../assets/images/workana-logo.png" alt="">
+            <ul class="links">
+                <li><RouterLink to="/">Encontre trabalho</RouterLink></li>
+                <li><RouterLink to="/">Meus projetos</RouterLink></li>
+                <li><RouterLink to="/">Minhas finanças</RouterLink></li>
+            </ul>
+        </div>
+        <div class="header-rightSide">
+            <div class="icons">
+                <img src="../../assets/images/search-icon.png" alt="">
+                <img id="chat-icon" src="../../assets/images/chat-icon.webp" alt="">
+                <img src="../../assets/images/crown-icon.webp" alt="">
+                <img src="../../assets/images/usuario-de-perfil.png" alt="">
+            </div>
+        </div>
+    </header>
+</template>
+<style scoped>
+@import '../../assets/Sass/default/_defaultHeader.scss';
+</style>
