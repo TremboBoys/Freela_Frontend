7cbb623e7432e262e6c065c8e04af16bc982ba25
dnzlucas <lucasdefenix@gmail.com>
2024-09-02 16:51:16 -0300

    FEAT: Add sign-in container layout in SignInUserView.vue. #10

--- src/views/user/SignInUserView.vue
+++ src/views/user/SignInUserView.vue
@@ -2,8 +2,13 @@
 </script>
 
 <template>
-    <div></div>
+    <div class="signIn-container">
+        <div class="left-side"></div>
+        <div class="right-side"></div>
+    </div>
+    
 </template>
 
 <style scoped>
+@import '../../assets/Sass/signIn/_signIn.scss';
 </style>
