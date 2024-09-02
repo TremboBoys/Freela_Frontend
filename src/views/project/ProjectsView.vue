c5a64a8ac4539594d000c8c1a90001202bb2d7db
SPNicolas <nicolassteffenspereira@gmail.com>
2024-09-02 16:51:52 -0300

    FEAT: Page project started #12

--- src/views/project/ProjectsView.vue
+++ src/views/project/ProjectsView.vue
@@ -1,9 +1,21 @@
 <script setup>
+
 </script>
 
 <template>
-    <div></div>
+    <div class="project">
+        <div class="search">
+                <p>
+                    pesquisar
+                </p>
+        </div>
+        <h1> 
+            Projetos em minha área 
+        </h1>
+        
+    </div>
 </template>
 
 <style scoped>
+@import '@/assets/Sass/project/_project.scss'
 </style>
