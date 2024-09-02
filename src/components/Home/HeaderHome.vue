cae336bf65dc9f346b52682b2ba0448f3b4dac6f
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-02 16:48:08 -0300

    FEAT: Create header home #11.

---
+++ src/components/Home/HeaderHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="@/assets/images/workana-logo.png" alt="Logo" />
+    </div>
+    <nav>
+      <p>Já tem uma conta?</p>
+      <router-link class="router" to="/sign-in">
+        <button class="button">Entrar</button>
+      </router-link>
+    </nav>
+  </header>
+</template>
+<style scoped>
+@import "@/assets/Sass/Home/_headerHome.scss";
+</style>
--- src/views/global/HomeView.vue
+++ src/views/global/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup>
+import HeaderHome from '@/components/Home/HeaderHome.vue';
 </script>
 
 <template>
-    <div></div>
+    <div>
+        <HeaderHome/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iusto, excepturi molestiae repudiandae doloribus consequatur, sunt vitae dolorem praesentium totam labore animi reiciendis. Illo quasi asperiores saepe sapiente, iure dolor.
+    </div>
 </template>
 
 <style scoped>
