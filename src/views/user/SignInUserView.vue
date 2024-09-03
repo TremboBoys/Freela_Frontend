5ffedca18b0b930be539d1d0121844e318563460
dnzLucas <lucasdiniz1708@gmail.com>
2024-09-03 15:22:55 -0300

    CHORE: Update sign-in container layout in SignInUserView.vue and _signIn.scss. #10

--- src/views/user/SignInUserView.vue
+++ src/views/user/SignInUserView.vue
@@ -1,12 +1,40 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <div class="signIn-container">
-        <div class="left-side"></div>
-        <div class="right-side"></div>
+  <div class="signIn-container">
+    <div class="left-side">
+      <img src="../../assets/images/workana-white-logo.png" alt="" />
+      <div class="login-container">
+        <h1>Login</h1>
+        <p>Seja bem vindo novamente!</p>
+
+        <div class="inputs-container">
+          <div class="form-group inputBox">
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder=" "
+            />
+            <label class="email labelInput" for="email">Email</label>
+          </div>
+          <div class="form-group inputBox">
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder=" "
+            />
+            <label class="email labelInput" for="email">Senha</label>
+          </div>
+          <p>Não possui uma conta? Cadastre-se</p>
+        </div>
+      </div>
     </div>
-    
+    <div class="right-side"></div>
+  </div>
 </template>
 
 <style scoped>
