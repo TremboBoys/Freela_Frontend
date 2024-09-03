42a14bcb1cb0696b76ec3e0039b454cd9103b182
SPNicolas <nicolassteffenspereira@gmail.com>
2024-09-03 15:21:45 -0300

    CHORE: styled code #12

--- src/views/project/ProjectsView.vue
+++ src/views/project/ProjectsView.vue
@@ -5,17 +5,24 @@
 <template>
     <div class="project">
         <div class="search">
-                <p>
-                    pesquisar
-                </p>
+                <div class="inputSearch">
+                    <div class="form-group inputBox">
+                        <input type="text" class="form-control" id="pesquisar" aria-describedby="pesquisarHelp" placeholder=" ">
+                        <label class="pesquisar labelInput" for="pesquisar">Pesquisar</label>
+                    </div>
+                </div>
+                <div class="categories">
+                    <div class="category">
+                        <label for="checkbox"></label>
+                    </div>
+                </div>
         </div>
-        <h1> 
-            Projetos em minha área 
-        </h1>
+        <h1> Projetos em minha área </h1>
+        
         
     </div>
 </template>
 
 <style scoped>
-@import '@/assets/Sass/project/_project.scss'
+@import '@/assets/Sass/project/_project.scss';
 </style>
