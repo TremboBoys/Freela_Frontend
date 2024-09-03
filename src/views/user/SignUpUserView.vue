4aeb6d97fdfb9c9b19def0eae83986a4e1fa88b5
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-09-03 15:16:19 -0300

    CHORE: Update SignUp page styling and add font import

--- src/views/user/SignUpUserView.vue
+++ src/views/user/SignUpUserView.vue
@@ -1,26 +1,38 @@
 <script setup></script>
 <template>
     <main>
-    <div class="container">
-        <header>
-            <img src="@/assets/images/workana-logo.png" alt="">
-        </header>
-        <form>
-            <h1>Cadastro</h1>
-            <input type="text" placeholder="Name">
-            <input type="email" placeholder="Email">
-            <input type="password" placeholder="Password">
-            <input type="password" placeholder="Confirm Password">
-            <p>Já possui uma conta? <router-link to="/login">Entrar</router-link></p>
-            <button>Finalizar o cadastro</button>
-        </form>
-     
-     
-     
-    </div>
+        <div class="container">
+            <header>
+                <img src="@/assets/images/workana-logo.png" alt="">
+            </header>
+            <form>
+                <h1>Cadastro</h1>
+                <div class="inputBox">
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
+                    <label class="email labelInput" for="email">Email</label>
+                </div>
+                <div class="inputBox">
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
+                    <label class="email labelInput" for="email">Nome de usuário</label>
+                </div>
+                <div class="inputBox">
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
+                    <label class="email labelInput" for="email">Senha</label>
+                </div>
+                <div class="inputBox">
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
+                    <label class="email labelInput" for="email">Confirmar Senha</label>
+                </div>
+                <p>Já possui uma conta? <router-link class="router" to="/login">Entrar</router-link></p>
+                <button>Finalizar o cadastro</button>
+            </form>
+
+
+
+        </div>
     </main>
 </template>
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap');
+
 @import '../../assets/Sass/SignUp/_SignUp.scss';
 </style>
