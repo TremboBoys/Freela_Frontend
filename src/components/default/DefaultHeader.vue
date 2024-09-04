5f04b6e392bdbdc6230d3e1a26376076e22bb9e2
dnzLucas <lucasdiniz1708@gmail.com>
2024-09-04 11:45:28 -0300

    REFACTOR: Update image sources in DefaultHeader.vue #6

--- src/components/default/DefaultHeader.vue
+++ src/components/default/DefaultHeader.vue
@@ -14,8 +14,8 @@ import { RouterLink } from 'vue-router';
         <div class="header-rightSide">
             <div class="icons">
                <RouterLink to="/"><img src="../../assets/images/search-icon.png" alt=""></RouterLink>
-               <RouterLink to="/"><img src="../../assets/images/chat-icon.webp" alt=""></RouterLink>
-               <RouterLink to="/"><img src="../../assets/images/crown-icon.webp" alt=""></RouterLink>
+               <RouterLink to="/"><img src="../../assets/images/chat-icon.png" alt=""></RouterLink>
+               <RouterLink to="/"><img src="../../assets/images/crown-icon.png" alt=""></RouterLink>
                <RouterLink to="/"><img src="../../assets/images/usuario-de-perfil.png" alt=""></RouterLink>
             </div>
         </div>
