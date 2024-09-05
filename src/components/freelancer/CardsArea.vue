41196de07c7a4db1a0f6331f64c18845158a5c26
dnzLucas <lucasdefenix@gmail.com>
2024-09-05 18:28:36 -0300

    FEAT: Create Search component and cards component #23

---
+++ src/components/freelancer/CardsArea.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import FreelancerCard from './FreelancerCard.vue';
+
+</script>
+
+<template>
+  <div class="cards-container">
+    <h1>Freelancers na minha área</h1>
+    <div class="cards">
+        <FreelancerCard/>
+        <FreelancerCard/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '../../assets/Sass/freelancer/_cardsArea.scss';
+</style>
--- src/views/freelancer/FreelancerView.vue
+++ src/views/freelancer/FreelancerView.vue
@@ -1,9 +1,15 @@
 <script setup>
+import SearchArea from '@/components/freelancer/SearchArea.vue';
+import CardsArea from '@/components/freelancer/CardsArea.vue';
 </script>
 
 <template>
-    <div></div>
+  <div class="freelancer-container">
+    <SearchArea/>
+    <CardsArea/>
+  </div>
 </template>
 
 <style scoped>
+@import '../../assets/Sass/freelancer/_freelancer.scss';
 </style>
