9760f603edc89c74017ac6b979f0a4f2aa5d1f03
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-05 21:30:11 -0300

    CHORE: FInish Dashboard page #22.

---
+++ src/components/dashboard/FavoriteArea.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <div class="text">
+            <p>Minha área favorita: <router-link to="/projects" class="link">Acesse aqui!</router-link></p>
+        </div>
+    </div>
+</template>
+<style scoped>
+@import '@/assets/Sass/dashboard/_favoriteArea.scss';
+</style>
--- src/views/global/DashboardView.vue
+++ src/views/global/DashboardView.vue
@@ -2,6 +2,9 @@
 import LogoName from '@/components/dashboard/LogoName.vue';
 import InfoProfile from '@/components/dashboard/InfoProfile.vue';
 import PublicProfile from '@/components/dashboard/PublicProfile.vue';
+import MainSkills from '@/components/dashboard/MainSkills.vue';
+import FavoriteArea from '@/components/dashboard/FavoriteArea.vue';
+import AboutMe from '@/components/dashboard/AboutMe.vue';
 </script>
 
 <template>
@@ -9,6 +12,9 @@ import PublicProfile from '@/components/dashboard/PublicProfile.vue';
         <LogoName />
         <InfoProfile />
         <PublicProfile />
+        <MainSkills />
+        <FavoriteArea />
+        <AboutMe />
     </div>
 </template>
 
