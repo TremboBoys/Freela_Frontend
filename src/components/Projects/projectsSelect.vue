a77b2d143908f4682a17751c180022189ffadf8e
SPNicolas <nicolassteffenspereira@gmail.com>
2024-09-06 23:14:27 -0300

    CHORE: styled "selectProject" component #12

--- src/components/Projects/projectsSelect.vue
+++ src/components/Projects/projectsSelect.vue
@@ -11,8 +11,36 @@
                 <p>R$2.000 - 5.000</p>
             </div>
         </div>
+        <div class="select-main-container">
+            <div class="select-subTitle">
+                <p>Publicado: há 10 horas</p>
+                <p>Prazo de entrega: 30/09/2024</p>
+                <p>Propostas: 10</p>
+            </div>
+            <div class="select-text">
+                <p>Seria uma empresa que vende mudanças residenciais e comerciais para todo o Brasil principalmente RJ e SP a pessoa teria acesso a um numero de Whatsapp e iria tentar fechar essa mudança com o cliente mostrando quais são nossas vantagens e tentando superar a concorrência. Com bônus de comissão por venda</p>
+            </div>
+            <div class="select-bottom-container">
+                <div class="select-bottom">
+                    <p>Web Design</p>
+                </div>
+                <div class="select-bottom">
+                    <p>Javascript</p>
+                </div>
+                <div class="select-bottom">
+                    <p>CSS</p>
+                </div>
+            </div>
+        </div>
+        <div class="select-footer-container">
+            <div class="select-footer-img">
 
-
+            </div>
+            <div class="select-footer-descrition">
+                <p>N. S. P.</p>
+                <p>Ultima resposta: há 8 horas</p>
+            </div>
+        </div>
     </div>
 </template>
 <style scoped>
