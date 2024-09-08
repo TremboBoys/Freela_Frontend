43ca33a5088ce3b37ddc5a018296d7c4bca3e2ab
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-09-08 11:58:32 -0300

    FEAT: Add SignUp page components, styles and installation of tailwind

---
+++ src/components/SignUp/SignUpButton.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+    <button class="register">Finalizar o cadastro</button>
+</template>
+<style scoped>
+ @import '../../assets/Sass/SignUp/_signUp-button.scss';
+</style>
--- src/views/user/SignUpUserView.vue
+++ src/views/user/SignUpUserView.vue
@@ -1,38 +1,8 @@
-<script setup></script>
+<script setup>
+  import SignUp from '@/components/SignUp/SignUp.vue';
+</script>
 <template>
-    <main>
-        <div class="container">
-            <header>
-                <img src="@/assets/images/workana-logo.png" alt="">
-            </header>
-            <form>
-                <h1>Cadastro</h1>
-                <div class="inputBox">
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
-                    <label class="email labelInput" for="email">Email</label>
-                </div>
-                <div class="inputBox">
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
-                    <label class="email labelInput" for="email">Nome de usuário</label>
-                </div>
-                <div class="inputBox">
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
-                    <label class="email labelInput" for="email">Senha</label>
-                </div>
-                <div class="inputBox">
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
-                    <label class="email labelInput" for="email">Confirmar Senha</label>
-                </div>
-                <p>Já possui uma conta? <router-link class="router" to="/login">Entrar</router-link></p>
-                <button>Finalizar o cadastro</button>
-            </form>
-
-
-
-        </div>
-    </main>
+    <SignUp />
 </template>
 <style scoped>
-
-@import '../../assets/Sass/SignUp/_SignUp.scss';
 </style>
