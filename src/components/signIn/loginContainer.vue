c31e9ec39eccdae2a61b61573137c36b07322c8c
dnzLucas <lucasdiniz1708@gmail.com>
2024-09-10 15:13:18 -0300

    REFACTOR:  Componentization in Login page #31

---
+++ src/components/signIn/loginContainer.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import SignInButton from '../../components/signIn/button.vue'
+import Icons from '../../components/signIn/icons.vue'
+import Inputs from '@/components/signIn/inputs.vue'
+import Intro from '@/components/signIn/intro.vue'
+import CreateAccount from '@/components/signIn/createAccount.vue'
+import forgetPassword from './forgetPassword.vue'
+
+</script>
+
+<template>
+  <div class="login-container">
+    <SignInIntro />
+    <div class="inputs-container">
+      <Intro />
+      <Inputs />
+      <forgetPassword/>
+      <CreateAccount />
+      <SignInButton />
+      <Icons />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '../../assets/Sass/signIn/_loginContainer.scss';
+</style>
--- src/views/user/SignInUserView.vue
+++ src/views/user/SignInUserView.vue
@@ -1,26 +1,14 @@
 <script setup>
-import SignInButton from '../../components/signIn/button.vue'
-import Icons from '../../components/signIn/icons.vue'
-import Inputs from '@/components/signIn/inputs.vue'
-import Intro from '@/components/signIn/intro.vue'
-import CreateAccount from '@/components/signIn/createAccount.vue'
+import loginContainer from '../../components/signIn/loginContainer.vue'
 </script>
 
 <template>
   <div class="signIn-container">
     <div class="left-side">
       <img src="../../assets/images/workana-white-logo.png" alt="" />
-      <div class="login-container">
-        <SignInIntro />
-        <div class="inputs-container">
-          <Intro/>
-          <Inputs />
-          <CreateAccount/>
-          <SignInButton/>
-          <Icons />
-        </div>
-      </div>
+      <loginContainer/>
     </div>
+
     <div class="right-side"></div>
   </div>
 </template>
