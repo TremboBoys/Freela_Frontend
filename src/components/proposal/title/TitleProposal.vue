8cc5d117ba67c3365c60fa5798d0209b7bac6501
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-09 21:58:07 -0300

    CHORE: Add title in component #33.

--- src/components/proposal/title/TitleProposal.vue
+++ src/components/proposal/title/TitleProposal.vue
@@ -1,3 +1,8 @@
 <template>
-    
+    <div class="container">
+        <h1>Proposta ao cliente</h1>
+    </div>
 </template>
+<style scoped>
+@import '@/assets/Sass/proposal/title/_titleProposal.scss';
+</style>
