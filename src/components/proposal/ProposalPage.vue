112b7fc9439cead4b5cc28da30281bc1d559e934
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-09 21:58:07 -0300

    CHORE: Add title in component #33.

--- src/components/proposal/ProposalPage.vue
+++ src/components/proposal/ProposalPage.vue
@@ -1,5 +1,10 @@
+<script setup>
+import Title from '@/components/proposal/title/TitleProposal.vue'
+</script>
 <template>
     <div class="container">
-        
+        <div class="header">
+            <Title />
+        </div>
     </div>
 </template>
