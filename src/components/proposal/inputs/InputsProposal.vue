38681c22622f0f481057b39cf01d4d5f5916ce22
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-12 15:56:03 -0300

    CHORE: New input add #33.

--- src/components/proposal/inputs/InputsProposal.vue
+++ src/components/proposal/inputs/InputsProposal.vue
@@ -1,8 +1,10 @@
 <script setup>
 import InputTextProposal from './InputTextProposal.vue';
+import InputSelectProposal from './InputSelectProposal.vue';
 </script>
 <template>
     <div class="container">
         <InputTextProposal />
+        <InputSelectProposal />
     </div>
 </template>
