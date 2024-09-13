dc7e92827155eb7a56dd737fa69e154189f360ad
lucasdefenix@gmail.com <lucasdefenix@gmail.com>
2024-09-13 15:12:01 -0300

    FEAT: Create subcomponents in sideBar component #44

---
+++ src/components/configProfile/SideBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+<template>
+    <div class="sideBar">
+        
+    </div>
+</template>
+
+<style scoped>
+@import "../../assets/Sass/configProfile/sideBar.scss";
+
+</style>
