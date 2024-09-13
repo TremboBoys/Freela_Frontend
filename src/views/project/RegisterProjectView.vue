40f63c65008974191ceb21ae822ccb08d16e8e32
SPNicolas <nicolassteffenspereira@gmail.com>
2024-09-13 15:11:59 -0300

    FEAT: project-registration page started #45

--- src/views/project/RegisterProjectView.vue
+++ src/views/project/RegisterProjectView.vue
@@ -1,9 +1,49 @@
 <script setup>
 </script>
-
 <template>
-    <div></div>
+    <div class="main-container">
+        <div class="design">
+            <input type="text" class="input">
+        </div>
+        <div class="resgister">
+            <h1>Cadastre seu projeto</h1>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.main-container {
+    display: flex;
+}
+
+.design {
+    margin: 67px 124px 40px 80px;
+    width: 308px;
+    height: 825px;
+    flex-shrink: 0;
+    border-radius: 11px;
+    border: 1px solid #D9D9D9;
+    background: #FFF;
+}
+
+.input {
+    margin: 46px 16px 32px 16px;
+    width: 276px;
+    height: 50px;
+    flex-shrink: 0;
+    fill: #FFF;
+    stroke-width: 1px;
+    stroke: #D9D9D9;
+    border-radius: 10px;
+    border: 1px solid #D9D9D9;
+    padding: 16px;
+    color: #000;
+    -webkit-text-stroke-width: 5;
+    -webkit-text-stroke-color: #FFF;
+    font-family: Poppins;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 275;
+    line-height: normal;
+}
 </style>
