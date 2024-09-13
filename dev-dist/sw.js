a832ec1e6c1c42108e035af8cebbfd2f23242fe9
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-13 15:12:44 -0300

    TEST: PWA added, but it is not working yet as it is not yet deployed.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v554hj9m0dg"
+    "revision": "0.250pd87uhgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
