1becef07c11cca1e95f59df86a527c7b6811b98e
lucasdefenix@gmail.com <lucasdefenix@gmail.com>
2024-09-13 15:12:01 -0300

    FEAT: Create subcomponents in sideBar component #44

--- src/views/user/ConfigurationUserView.vue
+++ src/views/user/ConfigurationUserView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import ConfigProfile from "../../components/configProfile/ConfigProfile.vue"
 </script>
 
 <template>
-    <div></div>
+    <ConfigProfile/>
 </template>
 
 <style scoped>
+
 </style>
