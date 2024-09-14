ed9b743066d64348ece23cbfcebc1171133d5b25
dnzlucas <lucasdefenix@gmail.com>
2024-09-14 10:55:32 -0300

    FEAT: Create components in config profile and add icons #44

--- src/components/configProfile/ConfigProfile.vue
+++ src/components/configProfile/ConfigProfile.vue
@@ -1,11 +1,12 @@
 <script setup>
 import SideBar from './SideBar.vue';
+import PersonalInformation from './personalInformation/PersonalInformation.vue';
 </script>
 
 <template>
-
     <div class="configProfile-container">
         <SideBar/>
+        <PersonalInformation/>
     </div>
 </template>
 
---
+++ src/components/configProfile/personalInformation/PersonalInformation.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div class="personal-container">
+        <h1>Informações pessoais</h1>
+    </div>
+</template>
+
+<style scoped>
+    @import "../../assets/Sass/configProfile/_personalInformation.scss";
+</style>
