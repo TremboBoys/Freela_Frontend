bd05c1a439eec7bebb24bda73294e7e55e38706c
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-13 23:20:03 -0300

    CHORE: Adjust style in inputs, and create new inputNumber #33.

--- src/components/proposal/inputs/InputNumberProposal.vue
+++ src/components/proposal/inputs/InputNumberProposal.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="container">
-        
+        <div class="form-group inputBox">
+            <input type="number" class="form-control" id="valor" aria-describedby="valorHelp" placeholder=" ">
+            <label class="email labelInput" for="valor">Valor</label>
+        </div>
     </div>
 </template>
+<style scoped>
+@import '@/assets/Sass/proposal/inputs/_inputNumber.scss';
+</style>
