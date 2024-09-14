4be6af022e80fd673304e9ff76607d3d802cd195
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-13 23:20:03 -0300

    CHORE: Adjust style in inputs, and create new inputNumber #33.

--- src/components/proposal/inputs/InputSelectProposal.vue
+++ src/components/proposal/inputs/InputSelectProposal.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 
+const selectedActivity = ref('TI & Programação');
 const selectedLanguage = ref('Português');
 const selectedRole = ref('Front-end Developer');
 const roles = ['Front-end Developer', 'Back-end Developer', 'Full-stack Developer', 'Designer', 'DevOps'];
 const languages = [
     "Mandarim", "Inglês", "Hindi", "Espanhol", "Francês", "Árabe", "Bengali", "Russo", "Português", "Urdu"
 ];
+const activitys = ["TI & Programação", "Design & Multimedia", "Tradução & Conteúdos", "Marketing & Vendas", "Suporte Administrativo"];
 </script>
 <template>
     <div class="dropdown">
@@ -23,6 +25,15 @@ const languages = [
         <div class="select-container">
             <select id="language" v-model="selectedLanguage">
                 <option v-for="language in languages" :key="language" :value="language">{{ language }}</option>
+            </select>
+            <span class="arrow">▼</span>
+        </div>
+    </div>
+    <div class="dropdown dropdown-2">
+        <label for="activity">Atividade Freelancer</label>
+        <div class="select-container">
+            <select id="activity" v-model="selectedActivity">
+                <option v-for="activity in activitys" :key="activity" :value="activity">{{ activity }}</option>
             </select>
             <span class="arrow">▼</span>
         </div>
--- src/components/proposal/inputs/InputsProposal.vue
+++ src/components/proposal/inputs/InputsProposal.vue
@@ -1,10 +1,21 @@
 <script setup>
 import InputTextProposal from './InputTextProposal.vue';
 import InputSelectProposal from './InputSelectProposal.vue';
+import InputPayment from './InputPayment.vue';
+import InputNumberProposal from './InputNumberProposal.vue';
 </script>
 <template>
     <div class="container">
-        <InputTextProposal />
-        <InputSelectProposal />
+        <div class="left">
+            <InputTextProposal />
+            <InputPayment />
+            <InputNumberProposal />
+        </div>
+        <div class="right">
+            <InputSelectProposal />
+        </div>
     </div>
 </template>
+<style scoped>
+
+</style>
