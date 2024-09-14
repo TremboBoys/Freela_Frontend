15a5ffc6141cf4c47f8f3d6a87169f624c200363
dnzlucas <lucasdefenix@gmail.com>
2024-09-14 10:55:32 -0300

    FEAT: Create components in config profile and add icons #44

--- src/components/configProfile/SideBar.vue
+++ src/components/configProfile/SideBar.vue
@@ -3,11 +3,32 @@
 </script>
 <template>
     <div class="sideBar">
-        
+        <div class="tabs-area">
+            <div class="tab">
+                <img src="../../assets/images/configProfile/person-icon.png" alt="">
+                <p>Informações pessoais</p>
+            </div>
+            <div class="tab">
+                <img src="../../assets/images/configProfile/person-icon.png" alt="">
+                <p>Informações pessoais</p>
+            </div>
+            <div class="tab">
+                <img src="../../assets/images/configProfile/person-icon.png" alt="">
+                <p>Informações pessoais</p>
+            </div>
+            <div class="tab">
+                <img src="../../assets/images/configProfile/person-icon.png" alt="">
+                <p>Informações pessoais</p>
+            </div>
+            <div class="tab">
+                <img src="../../assets/images/configProfile/person-icon.png" alt="">
+                <p>Informações pessoais</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-@import "../../assets/Sass/configProfile/sideBar.scss";
+@import "../../assets/Sass/configProfile/_sideBar.scss";
 
 </style>
