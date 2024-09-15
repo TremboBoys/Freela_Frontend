fcd7e71b891da344737ab59ead6f2de6735094a4
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-15 15:36:21 -0300

    FEAT: About Us page finish #51.

---
+++ src/components/aboutUs/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="title">
+                <h2>O que fazemos?</h2>
+            </div>
+            <div class="text">
+                <p class="description">Somos uma empresa pequena que está em constante evolução. Estamos iniciando nosso sistema com suporte somente para a América Latina, mas no futuro isso vai mudar, expandindo acesso a uma enorme quantidade de países do mundo. Estamos localizados no Sul do Brasil. Nossa equipe se expande com 6 integrantes. Temos o foco de revolucionar as idéias e modelos de freelancers, nossa idéia é ajudar os clientes a encontrarem freelancers que sejam competentes na área que ele precisa, estamos filtrando nossos freelancers e deixando somente quem tem competência e responsabilidade de assumir um projeto.</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+@import '@/assets/Sass/aboutUs/_mainPage.scss';
+</style>
