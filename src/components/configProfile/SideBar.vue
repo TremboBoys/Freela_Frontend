987c45ef31e7179a26f3f8e943cbbafaf0bfe6f3
dnzLucas <lucasdefenix@gmail.com>
2024-09-21 22:38:53 -0300

    CHORE: Add two components in ConfigProfile #44

--- src/components/configProfile/SideBar.vue
+++ src/components/configProfile/SideBar.vue
@@ -4,26 +4,26 @@
 <template>
     <div class="sideBar">
         <div class="tabs-area">
-            <div class="tab">
+            <RouterLink to="/"  class="tab">
                 <img src="../../assets/images/configProfile/person-icon.png" alt="">
                 <p>Informações pessoais</p>
-            </div>
-            <div class="tab">
-                <img src="../../assets/images/configProfile/person-icon.png" alt="">
-                <p>Informações pessoais</p>
-            </div>
-            <div class="tab">
-                <img src="../../assets/images/configProfile/person-icon.png" alt="">
-                <p>Informações pessoais</p>
-            </div>
-            <div class="tab">
-                <img src="../../assets/images/configProfile/person-icon.png" alt="">
-                <p>Informações pessoais</p>
-            </div>
-            <div class="tab">
-                <img src="../../assets/images/configProfile/person-icon.png" alt="">
-                <p>Informações pessoais</p>
-            </div>
+            </RouterLink>
+            <RouterLink to="/"  class="tab">
+                <img src="../../assets/images/configProfile/padlock-icon.png" alt="">
+                <p>Senha</p>
+            </RouterLink>
+            <RouterLink to="/"  class="tab">
+                <img src="../../assets/images/configProfile/email-icon.png" alt="">
+                <p>Email</p>
+            </RouterLink>
+            <RouterLink to="/"  class="tab">
+                <img src="../../assets/images/configProfile/medal-icon.png" alt="">
+                <p>Plano de assinatura</p>
+            </RouterLink>
+            <RouterLink to="/"  class="tab">
+                <img src="../../assets/images/configProfile/security-icon.png" alt="">
+                <p>Segurança</p>
+            </RouterLink>
         </div>
     </div>
 </template>
