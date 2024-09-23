4b7c4118dd048236de6b00ca9334c004fcd73745
LuizBlume <luiz.fernando.251107@gmail.com>
2024-09-23 15:43:26 -0300

    CHORE: Updating in component of contactUser #57.

---
+++ src/components/chat/contacts/ContactUser.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure odio illum laborum cum, maxime alias sapiente eaque ad sint odit voluptate velit accusantium? Ab non minus ex sequi, inventore est.
+    </div>
+</template>
+<style scoped>
+@import '@/assets/Sass/chat/contacts/_contactUser.scss';
+</style>
