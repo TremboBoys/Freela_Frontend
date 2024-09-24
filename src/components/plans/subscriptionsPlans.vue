4b281d770e033dc94a08b469df2b285dbdb7217b
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-09-24 15:08:59 -0300

    FEAT: Update subscription plans styles and layout #36

--- src/components/plans/subscriptionsPlans.vue
+++ src/components/plans/subscriptionsPlans.vue
@@ -1,9 +1,22 @@
 <script setup></script>
 <template>
     <div class="container">
-        <div class="plan"></div>
-        <div class="plan"></div>
-        <div class="plan"></div>
+        <div class="plan1">
+            <div class="image-title">
+                <div class="image"><img src="@/assets/images/Crown.png" alt=""></div>
+                <div class="title"><p class="text-title">Free</p></div>
+            </div>
+            <div class="benefits">
+                <ul>
+                    <li>1 User</li>
+                    <li>1 User</li>
+                    <li>1 User</li>
+                    <li>1 User</li>
+                </ul>
+            </div>
+        </div>
+        <div class="plan2"></div>
+        <div class="plan3"></div>
     </div>
 </template>
 <style scoped>   
