1413c1dd2e1d573d15c7261db875266a0633231d
SPNicolas <nicolassteffenspereira@gmail.com>
2024-09-24 15:01:05 -0300

    CHORE: page "RegisterProject" componentized #45

---
+++ src/components/registerProject/registerText.vue
@@ -0,0 +1,122 @@
+<script setup>
+
+</script>
+<template>
+   <div class="register">
+            <h1>Cadastre seu projeto</h1>
+            <div class="project">
+                <p class="text-register">Qual é o tema do seu projeto?</p>
+                <div class="text">
+                    <p>TI e Programação</p>
+                </div>
+                <button class="option">
+                    <p>Programação</p>
+                </button>
+                <button class="option">
+                    <p>Wordpress</p>
+                </button>
+                <button class="option">
+                    <p>Web design</p>
+                </button>
+                <button class="option">
+                    <p>Programação de Apps</p>
+                </button>
+                <button class="option">
+                    <p>Inteligência artificial</p>
+                </button>
+                <button class="option">
+                    <p>Outros</p>
+                </button>
+                <div class="text">
+                    <p>Design e Multimedia</p>
+                </div>
+                <button class="option">
+                    <p>Design de logo</p>
+                </button>
+                <button class="option">
+                    <p>Web design</p>
+                </button>
+                <button class="option">
+                    <p>Banners</p>
+                </button>
+                <button class="option">
+                    <p>Ilustrações</p>
+                </button>
+                <button class="option">
+                    <p>Criar ou editar vídeos</p>
+                </button>
+                <button class="option">
+                    <p>Infográficos</p>
+                </button>
+                <button class="option">
+                    <p>Outros</p>
+                </button>
+                <button class="option">
+                    <p>Design de app mobile</p>
+                </button>
+                <button class="option">
+                    <p>Imagens para redes sociais</p>
+                </button>
+                <button class="option">
+                    <p>Imagem corporativa</p>
+                </button>
+                <button class="option">
+                    <p>Outros</p>
+                </button>
+                <div class="text">
+                    <p>Marketing e vendas</p>
+                </div>
+                <button class="option">
+                    <p>SEO</p>
+                </button>
+                <button class="option">
+                    <p>Web design</p>
+                </button>
+                <button class="option">
+                    <p>Vendedor</p>
+                </button>
+                <button class="option">
+                    <p> Data Analytics</p>
+                </button>
+                <button class="option">
+                    <p>E-mail marketing</p>
+                </button>
+                <button class="option">
+                    <p>Community Management</p>
+                </button>
+                <button class="option">
+                    <p>Outros</p>
+                </button>
+                <div class="text">
+                    <p>Suporte Administrativo</p>
+                </div>
+                <button class="option">
+                    <p>Assistente virtual</p>
+                </button>
+                <button class="option">
+                    <p>Atenção ao cliente</p>
+                </button>
+                <button class="option">
+                    <p>Data entry</p>
+                </button>
+                <button class="option">
+                    <p>Investigação de mercado</p>
+                </button>
+                <button class="option">
+                    <p>Televendas</p>
+                </button>
+                <button class="option">
+                    <p>Inteligência Artificial</p>
+                </button>
+                <button class="option">
+                    <p>Outros</p>
+                </button>
+                <button class="continue">
+                    <p>Continuar</p>
+                </button>
+            </div>
+        </div>
+</template>
+<style scoped>
+@import '../../assets/Sass/project-registration/_project-registration.scss';
+</style>
