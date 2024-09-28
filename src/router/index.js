ea88b27ddd753879eee9e488ab94762becddca31
dnzLucas <lucasdefenix@gmail.com>
2024-09-28 15:51:23 -0300

    FEAT: Add background to active page tab #44

--- src/router/index.js
+++ src/router/index.js
@@ -39,8 +39,8 @@ const router = createRouter({
               component: EmailConfig
             },
             {
-              path: 'subscription-plan',
-              name: 'subscriptionPlan',
+              path: 'subscription',
+              name: 'subscription',
               component: SubscriptionPlan
             },
             {
