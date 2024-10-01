5276d809ab375750c51af05f241fd98d711589be
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-10-01 15:13:09 -0300

    FEAT: Update subscription plans styles and layout

--- src/components/plans/subscriptionsPlans.vue
+++ src/components/plans/subscriptionsPlans.vue
@@ -6,12 +6,13 @@
                 <div class="image"><img src="@/assets/images/Crown.png" alt=""></div>
                 <div class="title"><p class="text-title">Free</p></div>
             </div>
-            <div class="benefits">
+            <div class="benefits-list">
                 <ul>
-                    <li>1 User</li>
-                    <li>1 User</li>
-                    <li>1 User</li>
-                    <li>1 User</li>
+                    <li class="benefit"><span class="decoration">●</span>Propostas dentro de 48 horas</li>
+                    <li class="benefit"><span class="decoration">●</span>2 Projetos por vez</li>
+                    <li class="benefit"><span class="decoration">●</span>Propostas padrões</li>
+                    <li class="benefit"><span class="decoration">●</span>1 Projeto cadastrado por vez</li>
+                    <li class="benefit"><span class="decoration">●</span>1 Teste de competência por semana</li>
                 </ul>
             </div>
         </div>
