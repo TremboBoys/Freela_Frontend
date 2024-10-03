86b5e7c1447dfa7fa1b445f7fdb01d149e5ee03e
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-03 20:48:32 -0300

    CHORE: Finish report page #84.

--- src/components/report/ReportFreela.vue
+++ src/components/report/ReportFreela.vue
@@ -6,22 +6,21 @@ import DescriptionFreela from '@/components/report/DescriptionFreela.vue';
 import ButtonProject from '@/components/report/ButtonProject.vue';
 const data = reactive({
     titleFreela: 'Relatório',
-    titleProject: 'Agente de IA',
-
+    titleProject: 'Título do projeto',
 });
 
 </script>
 <template>
     <div class="content">
-        <TitleFreela />
-
+        <TitleFreela class="title" :title="data.titleFreela" />
         <TitleProject :title="data.titleProject" />
-
         <DescriptionFreela label="Descrição do Relatório" v-model="descricao" placeholder="Digite a descrição" />
-
         <ButtonProject @click="enviar" />
     </div>
 </template>
 <style scoped>
 @import '@/assets/Sass/report/_reportFreela.scss';
+.content {
+    color: #000 !important;
+}
 </style>
--- src/components/report/TitleProject.vue
+++ src/components/report/TitleProject.vue
@@ -1,10 +1,17 @@
 <script setup>
 const props = defineProps({
     title: String,
+    placeholder: String,
 })
 </script>
 <template>
     <div class="container">
-        <h1>{{ title }}</h1>
+        <h1 class="title">{{ title }}</h1>
+        <div class="input-field">
+            <input type="text" placeholder="Digite seu título" />
+        </div>
     </div>
 </template>
+<style scoped>
+@import '@/assets/Sass/report/_titleProject.scss';
+</style>
