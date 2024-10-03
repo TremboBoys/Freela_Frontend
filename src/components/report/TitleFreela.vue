0adff6e3fd57f6dd6f4c7e7888a649f87e459c8f
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-03 16:44:47 -0300

    FEAT: Add reusable components for report feature #84.

--- src/components/report/TitleFreela.vue
+++ src/components/report/TitleFreela.vue
@@ -1,5 +1,19 @@
+<script setup>
+const props = defineProps({
+  label: String,
+  modelValue: String,
+  placeholder: String,
+});
+const emit = defineEmits(['update:modelValue']);
+</script>
 <template>
     <div class="container">
-        <h1>Relatório</h1>
+        <div class="input-field">
+            <label>{{ label }}</label>
+            <input type="text" :placeholder="placeholder" />
+        </div>
     </div>
 </template>
+<style scoped>
+@import '@/assets/Sass/report/_titleFreela.scss';
+</style>
---
+++ src/components/report/TitleProject.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps({
+    title: String,
+})
+</script>
+<template>
+    <div class="container">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
