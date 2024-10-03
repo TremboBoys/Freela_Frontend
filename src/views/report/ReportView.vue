94865e994666f28acab29c56c792460caf1b3ec1
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-03 14:15:38 -0300

    FIX: Adjust in router and create file ReportView #84.

---
+++ src/views/report/ReportView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import ReportFreela from '@/components/report/ReportFreela.vue'
+</script>
+<template>
+    <div class="container">
+        <ReportFreela />
+    </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
