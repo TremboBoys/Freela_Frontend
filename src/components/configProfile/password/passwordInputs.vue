4b1204eb800eb5784542cb27a2e94687e69300cf
dnzlucas <lucasdefenix@gmail.com>
2024-10-04 15:17:08 -0300

    CHORE: Create inputs fot password page #77

---
+++ src/components/configProfile/password/passwordInputs.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <div class="passwordInputs">
+    <h1>Configuração de Senha</h1>
+    <div class="inputs">
+      <input type="password" value="teste" disabled />
+      <input type="password" />
+      <div class="form-group inputBox">
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+          placeholder=" "
+        />
+        <label class="email labelInput" for="email">Email</label>
+      </div>
+    </div>
+    <button></button>
+  </div>
+</template>
+
+<style scoped>
+@import '../../../assets/Sass/configProfile/_passwordInputs.scss';
+</style>
