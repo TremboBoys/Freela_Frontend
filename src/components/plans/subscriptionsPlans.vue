8c1ad2e619581350a820c6df88a8801c8a013588
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-10-04 15:16:09 -0300

    FEAT: update subscription plans layout and update benefits descriptions

--- src/components/plans/subscriptionsPlans.vue
+++ src/components/plans/subscriptionsPlans.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="benefits-list">
                 <ul>
-                    <li class="benefit"><span class="decoration">●</span>Propostas dentro de 48 horas</li>
+                    <li class="benefit"><span class="decoration">●</span>Propostas dentro de 12 horas</li>
                     <li class="benefit"><span class="decoration">●</span>2 Projetos por vez</li>
                     <li class="benefit"><span class="decoration">●</span>Propostas padrões</li>
                     <li class="benefit"><span class="decoration">●</span>1 Projeto cadastrado por vez</li>
@@ -16,8 +16,37 @@
                 </ul>
             </div>
         </div>
-        <div class="plan2"></div>
-        <div class="plan3"></div>
+        <div class="plan2">
+            <div class="image-title">
+                <div class="image"><img src="@/assets/images/Queen-Crown.png" alt=""></div>
+                <div class="title"><p class="text-title-month">Mensal</p></div>
+            </div>
+            <div class="benefits-list">
+                <ul>
+                    <li class="benefit"><span class="decoration">●</span>Propostas a qualquer momento</li>
+                    <li class="benefit"><span class="decoration">●</span>5 Projetos por vez</li>
+                    <li class="benefit"><span class="decoration">●</span>Propostas personalizadas</li>
+                    <li class="benefit"><span class="decoration">●</span>5 Projetos cadastrados por vez</li>
+                    <li class="benefit"><span class="decoration">●</span>2 Testes de competência por semana</li>
+                </ul>
+            </div>
+        </div>
+        <div class="plan3">
+            <div class="image-title">
+                <div class="image"><img src="@/assets/images/Medieval-Crown.png" alt=""></div>
+                <div class="title"><p class="text-title">Anual</p></div>
+                <div class="image"><img src="@/assets/images/discount.png" alt=""></div>
+            </div>
+            <div class="benefits-list">
+                <ul>
+                    <li class="benefit"><span class="decoration">●</span>Propostas a qualquer momento</li>
+                    <li class="benefit"><span class="decoration">●</span>Sem limite de projeto por vez</li>
+                    <li class="benefit"><span class="decoration">●</span>Propostas personalizadas</li>
+                    <li class="benefit"><span class="decoration">●</span>Projetos ilimitados cadastrados</li>
+                    <li class="benefit"><span class="decoration">●</span>3 Testes de competência por semana</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 <style scoped>   
