e1af8fc94c0346a9211d858ec46e0ba9cb0b032f
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-06 12:02:46 -0300

    CHORE: Aside contacts user finish #57.

--- src/components/chat/AsideContacts.vue
+++ src/components/chat/AsideContacts.vue
@@ -1,8 +1,17 @@
 <script setup>
+import ButtonSearch from '@/components/chat/contacts/ButtonSearch.vue'
 import ContactUser from './contacts/ContactUser.vue'
 </script>
 <template>
     <div class="container">
+        <ButtonSearch />
+        <ContactUser />
+        <ContactUser />
+        <ContactUser />
+        <ContactUser />
+        <ContactUser />
+        <ContactUser />
+        <ContactUser />
         <ContactUser />
     </div>
 </template>
---
+++ src/components/chat/contacts/ButtonSearch.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="search-bar">
+            <div class="menu-icon">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+            <input type="text" placeholder="Search" />
+            <div class="search-icon">
+                🔍
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+@import '@/assets/Sass/chat/contacts/_buttonSearch.scss';
+</style>
