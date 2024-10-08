50aaac2e7a8d64c771be6c154e930b46a593742c
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-08 15:16:03 -0300

    FIX: Adjust in chatMessage #57.

---
+++ src/components/chat/chatMessage/SelectedNone.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <div class="selected-none">
+            <p>Nothing selected</p>
+        </div>
+    </div>
+</template>
+<style scoped>
+@import '@/assets/Sass/chat/chatMessage/_selectedNone.scss';
+</style>
--- src/views/chat/ChatView.vue
+++ src/views/chat/ChatView.vue
@@ -1,12 +1,28 @@
 <script setup>
 import AsideContacts from '@/components/chat/AsideContacts.vue'
+import ChatMessage from '@/components/chat/ChatMessage.vue'
 </script>
 
 <template>
-    <div class="container">
+    <div class="aside-contacts">
         <AsideContacts />
+    </div>
+    <div class="chat-message">
+        <ChatMessage />
     </div>
 </template>
 
 <style scoped>
+
+.aside-contacts {
+    display: flex;
+    flex-direction: column;
+}
+.chat-message {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    overflow-y: auto;
+    padding: 0 20px;
+}
 </style>
