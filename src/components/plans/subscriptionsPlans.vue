336868999e1d0b1446cae0da471ed8c0ccf90b9e
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-10-08 15:12:30 -0300

    FEAT: Update subscription plans styles, layout and add buttons #36

--- src/components/plans/subscriptionsPlans.vue
+++ src/components/plans/subscriptionsPlans.vue
@@ -15,11 +15,14 @@
                     <li class="benefit"><span class="decoration">●</span>1 Teste de competência por semana</li>
                 </ul>
             </div>
+            <div class="sign">
+                <button>Já incluso</button>
+            </div>
         </div>
         <div class="plan2">
             <div class="image-title">
                 <div class="image"><img src="@/assets/images/Queen-Crown.png" alt=""></div>
-                <div class="title"><p class="text-title-month">Mensal</p></div>
+                <div class="title"><p class="text-title">Mensal</p></div>
         </div>
             <div class="benefits-list">
                 <ul>
@@ -27,24 +30,36 @@
                     <li class="benefit"><span class="decoration">●</span>5 Projetos por vez</li>
                     <li class="benefit"><span class="decoration">●</span>Propostas personalizadas</li>
                     <li class="benefit"><span class="decoration">●</span>5 Projetos cadastrados por vez</li>
-                    <li class="benefit"><span class="decoration">●</span>2 Testes de competência por semana</li>
+                    <li class="benefit"><span class="decoration">●</span>3 Testes de competência por semana</li>
+                    <li class="benefit"><span class="decoration">●</span>Insígnia PRO mensal no perfil</li>
+                    <li class="benefit"><span class="decoration">●</span>Destaque em propostas</li>
+                    <li class="benefit"><span class="decoration">●</span>Destaque em projetos</li>
                 </ul>
+            </div>
+            <div class="sign">
+                <button>R$30,00 mês</button>
             </div>
         </div>
         <div class="plan3">
             <div class="image-title">
                 <div class="image"><img src="@/assets/images/Medieval-Crown.png" alt=""></div>
                 <div class="title"><p class="text-title">Anual</p></div>
-                <div class="image"><img src="@/assets/images/discount.png" alt=""></div>
             </div>
             <div class="benefits-list">
                 <ul>
                     <li class="benefit"><span class="decoration">●</span>Propostas a qualquer momento</li>
-                    <li class="benefit"><span class="decoration">●</span>Sem limite de projeto por vez</li>
+                    <li class="benefit"><span class="decoration">●</span>5 Projetos por vez</li>
                     <li class="benefit"><span class="decoration">●</span>Propostas personalizadas</li>
-                    <li class="benefit"><span class="decoration">●</span>Projetos ilimitados cadastrados</li>
+                    <li class="benefit"><span class="decoration">●</span>5 Projetos cadastrados por vez</li>
                     <li class="benefit"><span class="decoration">●</span>3 Testes de competência por semana</li>
+                    <li class="benefit"><span class="decoration">●</span>Insígnia PRO anual no perfil</li>
+                    <li class="benefit"><span class="decoration">●</span>Destaque maior em propostas</li>
+                    <li class="benefit"><span class="decoration">●</span>Destaque maior em projetos</li>
+                    <li class="benefit"><span class="decoration">●</span>Pague 11, leve 12 meses</li>
                 </ul>
+            </div>
+            <div class="sign">
+                <button>R$25,00 mês</button>
             </div>
         </div>
     </div>
