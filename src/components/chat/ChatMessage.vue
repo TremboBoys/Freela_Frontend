8a0532bc99693ae7586f46a42ee342d5bddf38cb
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-08 15:16:03 -0300

    FIX: Adjust in chatMessage #57.

---
+++ src/components/chat/ChatMessage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import SelectedNone from './chatMessage/SelectedNone.vue'
+</script>
+<template>
+    <div class="container">
+        <SelectedNone />
+    </div>
+</template>
+<style scoped>
+</style>
