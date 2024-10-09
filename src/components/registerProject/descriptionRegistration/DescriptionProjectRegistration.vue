edc43f529213b5ce317d3b7b71fda222ddce13f0
SPNicolas <nicolassteffenspereira@gmail.com>
2024-10-09 09:31:57 -0300

    CHORE: Started page "DescriptionProjectRegistration" #96

--- src/components/registerProject/descriptionRegistration/DescriptionProjectRegistration.vue
+++ src/components/registerProject/descriptionRegistration/DescriptionProjectRegistration.vue
@@ -2,8 +2,21 @@
 </script>
 
 <template>
-    <div></div>
+    <div>
+        <div class="container-description">
+            <h1 class="cadastro">Cadastre seu projeto</h1>
+            <div class="header-description">
+                <p>Título do projeto</p>
+                <input type="text" class="text-description">
+            </div>
+            <div class="main-description">
+                <p>Descrição do projeto</p>
+                <input type="text" class="text-description">
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+@import '@/assets/Sass/project-registration/_description-project-registration.scss';
 </style>
