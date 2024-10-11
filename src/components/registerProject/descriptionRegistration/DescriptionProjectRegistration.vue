fe5f358091f6497f69a5122d27bf97593724e199
SPNicolas <nicolassteffenspereira@gmail.com>
2024-10-11 14:58:18 -0300

    CHORE: Page "DescriptionProject" completed #96

--- src/components/registerProject/descriptionRegistration/DescriptionProjectRegistration.vue
+++ src/components/registerProject/descriptionRegistration/DescriptionProjectRegistration.vue
@@ -2,17 +2,39 @@
 </script>
 
 <template>
-    <div>
+    <div class="container">
         <div class="container-description">
-            <h1 class="cadastro">Cadastre seu projeto</h1>
+            <h1 class="cadastro">
+                Cadastre seu projeto
+            </h1>
             <div class="header-description">
                 <p>Título do projeto</p>
                 <input type="text" class="text-description">
             </div>
             <div class="main-description">
                 <p>Descrição do projeto</p>
-                <input type="text" class="text-description">
+                <textarea class="text-description">
+                </textarea>
             </div>
+            <div class="main-description">
+                <p>Contexto geral do projeto</p>
+                <textarea class="text-description2">
+                </textarea>
+            </div>
+            <div class="footer-description">
+                <div class="footer-barra-description-verde"></div>
+                <div class="footer-barra-description"></div>
+                <div class="footer-barra-description"></div>
+                <div class="footer-barra-description-final"></div>
+            </div>
+            <div class="bottom">
+                <button>Continuar</button>
+            </div>
+            
+        </div>
+        <div class="box-notice">
+            <img src="../../../assets/images/alert-circle.png" alt="">
+            <p>Diga ao freelancer o objetivo do seu projeto. Adicione sites de referência</p>
         </div>
     </div>
 </template>
