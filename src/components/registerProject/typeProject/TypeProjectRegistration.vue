968c27cabfc793950bc4ae42cd162e27eac86f4c
SPNicolas <nicolassteffenspereira@gmail.com>
2024-10-11 15:17:44 -0300

    CHORE: page "TypeProjectRegistration" #96

--- src/components/registerProject/typeProject/TypeProjectRegistration.vue
+++ src/components/registerProject/typeProject/TypeProjectRegistration.vue
@@ -2,8 +2,13 @@
 </script>
 
 <template>
-    <div></div>
+    <div class="container">
+        <h1 class="cadastro">
+            Cadastre seu projeto
+        </h1>
+    </div>
 </template>
 
 <style scoped>
+@import '@/assets/Sass/project-registration/_types-project-registration.scss';
 </style>
