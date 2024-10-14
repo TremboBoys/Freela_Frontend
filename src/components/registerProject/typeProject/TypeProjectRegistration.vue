813e8dc12e284d495a5d61ad1f9dfd6e6367609f
SPNicolas <nicolassteffenspereira@gmail.com>
2024-10-14 16:55:29 -0300

    CHORE: page "TypesProjectRegistration" styled #96

--- src/components/registerProject/typeProject/TypeProjectRegistration.vue
+++ src/components/registerProject/typeProject/TypeProjectRegistration.vue
@@ -3,9 +3,36 @@
 
 <template>
     <div class="container">
-        <h1 class="cadastro">
-            Cadastre seu projeto
-        </h1>
+        <h1 class="cadastro">Cadastre seu projeto</h1>
+        <div class="main-option">
+            <p>Quais demandas o freelancer irá precisar desenvolver?</p>
+            <div class="options">
+                <div class="option">
+                <label>
+                    <input type="checkbox" />
+                    <p>Web Design</p>
+                </label>
+            </div>
+            <div class="option">
+                <label>
+                    <input type="checkbox" />
+                    Design mobile
+                </label>
+            </div>
+            <div class="option">
+                <label>
+                    <input type="checkbox" />
+                    Gerente
+                </label>
+            </div>
+            <div class="option">
+                <label>
+                    <input type="checkbox" />
+                    Outro
+                </label>
+            </div>
+            </div>
+        </div>
     </div>
 </template>
 
