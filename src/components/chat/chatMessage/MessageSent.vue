a8f9b5a7ccbe0409b255b008a7a750bb00ab7771
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-15 12:44:36 -0300

    CHORE: Update chat message container styles and add image in selected none component #57.

--- src/components/chat/chatMessage/MessageSent.vue
+++ src/components/chat/chatMessage/MessageSent.vue
@@ -8,8 +8,16 @@
                 <div class="avatar">
                     <img src="@/assets/images/dashboard/luiz.jpg" alt="">
                 </div>
-                <p class="text">Hello, how are you?</p>
-                <p class="time">10:00</p>
+                <div class="messages">
+                    <div class="message-text first-message">
+                        <p class="text">Hello, how are you?</p>
+                        <p class="time">10:00</p>
+                    </div>
+                    <div class="message-text">
+                        <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam minima aliquid similique nemo possimus placeat natus odit, cupiditate reiciendis explicabo molestias tenetur beatae officiis eum voluptates, necessitatibus ratione, repellat quisquam?</p>
+                        <p class="time">10:00</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
