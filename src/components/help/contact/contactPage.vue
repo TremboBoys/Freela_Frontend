23badb274655032aef0e4be7d3d5900e650e8132
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-10-15 15:07:04 -0300

    FEAT: Add contactPage component and styles to ContactView

--- src/components/help/contact/contactPage.vue
+++ src/components/help/contact/contactPage.vue
@@ -1,9 +1,17 @@
 <script setup></script>
 <template>
   <main>
-    <div></div>
+    <div class="image-title">
+      <img class="image" src="@/assets/images/tremboBoysLogo.png" alt="">
+      <div class="title">
+        <p class="text-title">Entre em contato com a nossa equipe</p>
+      </div>
+    </div>
+    <div class="subTitle">
+      <p>Encontre aqui toda a ajuda da comunidade e respostas às Perguntas Frequentes</p>
+    </div>
   </main>
 </template>
 <style scoped>
-   
+  @import '@/assets/Sass/help/contact/_contactPage.scss'
 </style>
