12d4837b0b6b7b953dd3d5a43f2b0804271f18ac
SPNicolas <nicolassteffenspereira@gmail.com>
2024-10-16 09:31:58 -0300

    CHORE: Page "SizeProjectRegistration" started #96

--- src/components/registerProject/sizeProject/SizeProjectRegistration.vue
+++ src/components/registerProject/sizeProject/SizeProjectRegistration.vue
@@ -3,7 +3,27 @@
 
 <template>
     <div class="container">
-        
+        <div class="header-size">
+            <h1>Cadastre seu projeto</h1>
+            <p class="sub-title">Últimos ajustes...</p>
+        </div>
+        <div class="main-size">
+            <p>Qual o nível de experiência que você busca?</p>
+            <div class="options-size">
+                <div class="option-size">
+                    <p>Iniciante</p>
+                </div>
+                <div class="option-size">
+                    <p>Intermediário</p>
+                </div>
+                <div class="option-size">
+                    <p>Experiente</p>
+                </div>
+                <div class="option-size">
+                    <p>Especialista</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
