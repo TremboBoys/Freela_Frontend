c42c012b78cfbfe11b57d51da65287c56bd1f1c4
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-16 09:32:55 -0300

    CHORE: Add new function and send messages is correct and finish #57.

--- src/components/chat/chatMessage/MessageSent.vue
+++ src/components/chat/chatMessage/MessageSent.vue
@@ -1,22 +1,32 @@
+<script setup>
+import { defineProps } from 'vue';
+
+// Recebe as mensagens enviadas como props
+defineProps({
+  messages: Array
+});
+</script>
+
 <template>
-    <div class="container-sent">
-        <div class="messageSent">
-            <div class="message">
-                <div class="avatar">
-                    <img src="@/assets/images/dashboard/luiz.jpg" alt="">
-                </div>
-                <div class="messages">
-                    <div class="message-text first-message">
-                        <p class="text">Hello, how are you?</p>
-                    </div>
-                    <div class="message-text">
-                        <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam minima aliquid similique nemo possimus placeat natus odit, cupiditate reiciendis explicabo molestias tenetur beatae officiis eum voluptates, necessitatibus ratione, repellat quisquam?</p>
-                    </div>
-                </div>
-            </div>
+  <div class="container-sent">
+    <div class="messageSent" v-for="(message, index) in messages" :key="index">
+      <div class="message">
+        <div class="avatar">
+          <img src="@/assets/images/dashboard/luiz.jpg" alt="">
         </div>
+        <div class="messages">
+          <div class="message-text" v-if="message.text">
+            <p class="text">{{ message.text }}</p>
+          </div>
+          <div class="message-text" v-if="message.file">
+            <p class="text">File: {{ message.file }}</p>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+
 <style scoped>
 @import '@/assets/Sass/chat/chatMessage/_messageSent.scss';
 </style>
--- src/components/chat/chatMessage/SendMessage.vue
+++ src/components/chat/chatMessage/SendMessage.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { ref } from 'vue';
+import MessageSent from './MessageSent.vue'; // Importando o componente de exibição
 
 const fileInput = ref(null);
 const file = ref(null);
+const message = ref('');
+const messages = ref([]); // Armazena todas as mensagens enviadas
 
 const triggerFileInput = () => {
     fileInput.value.click();
@@ -11,21 +14,46 @@ const triggerFileInput = () => {
 const handleFileUpload = (event) => {
     file.value = event.target.files[0];
 };
+
+const sendMessage = () => {
+    if (!message.value && !file.value) {
+        alert('Please enter a message or select a file');
+        return;
+    }
+
+    // Adiciona a mensagem e o arquivo ao array de mensagens
+    messages.value.push({
+        text: message.value,
+        file: file.value ? file.value.name : null, // Se houver arquivo, adiciona o nome
+    });
+
+    // Resetar os campos após o envio
+    message.value = '';
+    file.value = null;
+    fileInput.value.value = '';  
+};
 </script>
+
 <template>
     <div class="container">
         <div class="left">
             <input type="file" ref="fileInput" @change="handleFileUpload" style="display: none" />
-            <button @click="triggerFileInput" class="upload-btn"><i class="fa fa-plus"></i></button>
+            <button @click="triggerFileInput" class="upload-btn">
+                <i class="fa fa-plus"></i>
+            </button>
             <p v-if="file">File: {{ file.name }}</p>
         </div>
         <div class="center">
-            <input type="text" placeholder="Type a message" class="input-text" />
-            <button class="send-btn"><i class="fa fa-paper-plane"></i></button>
+            <input type="text" placeholder="Type a message" v-model="message" class="input-text" />
+            <button @click="sendMessage" class="send-btn">
+                <i class="fa fa-paper-plane"></i>
+            </button>
         </div>
+
+        <MessageSent :messages="messages" />
     </div>
 </template>
+
 <style scoped>
 @import '@/assets/Sass/chat/chatMessage/_sendMessage.scss';
 </style>
-  
