56be4eaa330be5d3ed81cd7b08eb0882de21cc03
SPNicolas <nicolassteffenspereira@gmail.com>
2024-10-23 09:16:51 -0300

    CHORE: Page "SizeProjectRegistration" styled and add button#96

--- src/components/registerProject/sizeProject/SizeProjectRegistration.vue
+++ src/components/registerProject/sizeProject/SizeProjectRegistration.vue
@@ -23,6 +23,45 @@
                     <p>Especialista</p>
                 </div>
             </div>
+            <p>Qual o tamanho do projeto?</p>
+            <div class="options-size">
+                <div class="option-size">
+                    <p>Pequeno</p>
+                </div>
+                <div class="option-size">
+                    <p>Intermediário</p>
+                </div>
+                <div class="option-size">
+                    <p>Grande</p>
+                </div>
+                <div class="option-size">
+                    <p>Gigante</p>
+                </div>
+            </div>
+            <p>Qual o orçamento que você deseja pagar?</p>
+            <div class="options-size">
+                <div class="option-size">
+                    <p>R$100 - 1.000</p>
+                </div>
+                <div class="option-size">
+                    <p>R$1.000 - 5.000</p>
+                </div>
+                <div class="option-size">
+                    <p>R$5.000 - 10.000</p>
+                </div>
+                <div class="option-size">
+                    <p>Mais de R$10.000</p>
+                </div>
+            </div>
+            <div class="entrega">
+                <label class="custom-checkbox">
+                    <span class="checkmark">
+                        <input type="radio" name="" id="">
+                    </span>
+                </label>
+                <p>Tenho um prazo de entrega</p>
+                <input class="date" type="date" name="" id="">
+            </div>
         </div>
     </div>
 </template>
