a129d659d51849c98c7a05b1f997dfe9518db09f
LuizBlume <luiz.fernando.251107@gmail.com>
2024-10-23 14:16:29 -0300

    FIX: Adjust in router-link header default and header home #118.

--- src/components/default/header/RightSide.vue
+++ src/components/default/header/RightSide.vue
@@ -6,10 +6,9 @@ import { RouterLink } from 'vue-router';
 <template>
     <div class="header-rightSide">
         <div class="icons">
-            <RouterLink to="/"><img src="../../../assets/images/search-icon.png" alt=""></RouterLink>
-            <RouterLink to="/"><img src="../../../assets/images/chat-icon.png" alt=""></RouterLink>
-            <RouterLink to="/"><img src="../../../assets/images/crown-icon.png" alt=""></RouterLink>
-            <RouterLink to="/"><img src="../../../assets/images/usuario-de-perfil.png" alt=""></RouterLink>
+            <RouterLink to="/chat"><img src="../../../assets/images/chat-icon.png" alt=""></RouterLink>
+            <RouterLink to="/subscription-plan"><img src="../../../assets/images/crown-icon.png" alt=""></RouterLink>
+            <RouterLink to="/dashboard"><img src="../../../assets/images/usuario-de-perfil.png" alt=""></RouterLink>
         </div>
     </div>
 </template>
--- src/components/default/header/dropdowns/FoundJob.vue
+++ src/components/default/header/dropdowns/FoundJob.vue
@@ -1,29 +1,30 @@
 <script setup>
 import { ref } from 'vue';
 const isDropdownOpen = ref(false);
-
 const jobLinks = [
-  { name: 'Encontre Projeto', path: '/' },
-  { name: 'Encontre projetos na minha área', path: '/' },
-  { name: 'Minha área favorita', path: '/' },
-  { name: 'Assinatura do Plano', path: '/' },
+  { name: 'Encontre Projeto', path: '/projects' },
+  { name: 'Encontre projetos na minha área', path: '/projects' },
+  { name: 'Minha área favorita', path: '/projects' },
+  { name: 'Assinatura do Plano', path: '/subscription-plan' },
 ]
 </script>
 
 <template>
-<div class="dropdown" @mouseover="isDropdownOpen = true" @mouseleave="isDropdownOpen = false">
+  <div class="dropdown" @mouseover="isDropdownOpen = true" @mouseleave="isDropdownOpen = false">
     <p class="dropdown-text">Encontre Trabalho</p>
-    
     <div v-show="isDropdownOpen" class="dropdown-menu">
-        <RouterLink class="routerlink" to="/" v-for="(link, index) in jobLinks" :key="index">{{ link.name }}</RouterLink>
-
+      <router-link 
+        v-for="(link, index) in jobLinks" 
+        :key="index" 
+        class="routerlink" 
+        :to="link.path"
+      >
+        {{ link.name }}
+      </router-link>
     </div>
   </div>
-  
 </template>
 
 <style scoped>
 @import '../../../../assets/Sass/default/header/_dropdown.scss';
-
-
 </style>
