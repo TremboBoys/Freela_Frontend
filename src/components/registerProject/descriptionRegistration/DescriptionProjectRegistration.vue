50f964b43ecbd7eacdb30bfe28de858edfd758dc
SPNicolas <nicolassteffenspereira@gmail.com>
2024-10-24 23:30:09 -0300

    CHORE: Created all pages #128

---
+++ src/components/registerProject/descriptionRegistration/DescriptionProjectRegistration.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container">
+        <div class="container-description">
+            <h1 class="cadastro">
+                Cadastre seu projeto
+            </h1>
+            <div class="header-description">
+                <p>Título do projeto</p>
+                <input type="text" class="text-description">
+            </div>
+            <div class="main-description">
+                <p>Descrição do projeto</p>
+                <textarea class="text-description">
+                </textarea>
+            </div>
+            <div class="main-description">
+                <p>Contexto geral do projeto</p>
+                <textarea class="text-description2">
+                </textarea>
+            </div>
+            <div class="footer-description">
+                <div class="footer-barra-description-verde"></div>
+                <div class="footer-barra-description"></div>
+                <div class="footer-barra-description"></div>
+                <div class="footer-barra-description-final"></div>
+            </div>
+            <div class="bottom">
+                <button>Continuar</button>
+            </div>
+            
+        </div>
+        <div class="box-notice">
+            <img src="../../../assets/images/alert-circle.png" alt="">
+            <p>Diga ao freelancer o objetivo do seu projeto. Adicione sites de referência</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import '@/assets/Sass/project-registration/_description-project-registration.scss';
+</style>
---
+++ src/components/registerProject/typeProject/TypeProjectRegistration.vue
@@ -0,0 +1,122 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="cadastro">Cadastre seu projeto</h1>
+        <div class="main-option">
+            <p>Quais demandas o freelancer irá precisar desenvolver?</p>
+            <div class="options">
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Web Design
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Design mobile
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Gerente
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Outro
+                    </label>
+                </div>
+            </div>
+            <p>Qual o nível de desenvolvimento você se encontra?</p>
+            <div class="options">
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Você tem o design
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Você tem uma ideia
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Não tenho nada
+                    </label>
+                </div>
+            </div>
+            <p>Você precisa de uma integração com API?</p>
+            <div class="options">
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Midias Sociais
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        API de pagamento
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Outras APIs
+                    </label>
+                </div>
+            </div>
+            <p>Você precisa de um recurso especial?</p>
+            <div class="options">
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Sim, bibliotecas
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Talvez
+                    </label>
+                </div>
+                <div class="option">
+                    <label>
+                        <input type="checkbox" />
+                        Não
+                    </label>
+                </div>
+            </div>
+            <div class="footer-types">
+                <div class="footer-barra-description-verde">
+
+                </div>
+                <div class="footer-barra-description-verde2">
+
+                </div>
+                <div class="footer-barra-description">
+
+                </div>
+                <div class="footer-barra-description-final">
+                    
+                </div>
+            </div>
+            <div class="bottom">
+                <button>Continuar</button>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped>
+@import '@/assets/Sass/project-registration/_types-project-registration.scss';
+</style>
