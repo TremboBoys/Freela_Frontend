5632a620d0e0418cb704d0bc4342395948959af7
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-10-29 15:06:48 -0300

    FEAT: add initial components #111

--- src/components/help/contact/contactPage.vue
+++ src/components/help/contact/contactPage.vue
@@ -1,28 +1,11 @@
-<script setup></script>
+<script setup>
+ import contactTitle from './contactTitle.vue';
+ import questionsHelp from './questionsHelp.vue';
+</script>
 <template>
   <main>
-    <div class="image-title">
-      <img class="image" src="@/assets/images/tremboBoysLogo.png" alt="">
-      <div class="title">
-        <p class="text-title">Entre em contato com a nossa equipe</p>
-      </div>
-    </div>
-    <div class="subTitle">
-      <p>Encontre aqui toda a ajuda da comunidade e respostas às Perguntas Frequentes</p>
-    </div>
-    <section>
-      <div class="questions">
-          <div class="questions-title">
-            <p class="title">Perguntas Frequentes</p>
-          </div>
-          <div class="questions-items">
-            <a class="question">oi?</a>
-            <a class="question">oi?</a>
-            <a class="question">oi?</a>
-            <a class="question">oi?</a>
-      </div>
-    </div>
-    </section>
+    <contactTitle />
+    <questionsHelp />
   </main>
 </template>
 <style scoped>
