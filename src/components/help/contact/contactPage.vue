bc7a41ebe3712eeb1f8830a81a0872c57afb59cf
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-11-01 14:54:03 -0300

    FEAT: update styles #111

--- src/components/help/contact/contactPage.vue
+++ src/components/help/contact/contactPage.vue
@@ -10,15 +10,15 @@
       <div class="help-title"><p>Central de ajuda</p></div>
       <div class="instructions">
         <div class="instruction">
-          <div class="number"><p>1</p></div>
+          <div class="number"><p>1)</p></div>
           <div class="text"><p>Selecione seu perfil de consulta</p></div>
         </div>
         <div class="instruction">
-          <div class="number"><p>2</p></div>
+          <div class="number"><p>2)</p></div>
           <div class="text"><p>Indique sua consulta no painel de ajuda</p></div>
         </div>
         <div class="instruction">
-          <div class="number"><p>3</p></div>
+          <div class="number"><p>3)</p></div>
           <div class="text"><p>Solução disponível para sua consulta</p></div>
         </div>
       </div>
