d22ec82503eb1e4db614034cdb91c98466d608a7
dnzlucas <lucasdefenix@gmail.com>
2024-11-01 16:56:16 -0300

    CHORE: Responsiveness for icons and dropdowns in header #132

--- src/components/default/header/RightSide.vue
+++ src/components/default/header/RightSide.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
 
+const isOpen = ref(false);
+
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value;
+};
 </script>
 
 <template>
@@ -9,6 +15,19 @@ import { RouterLink } from 'vue-router';
             <RouterLink to="/chat"><img src="../../../assets/images/chat-icon.png" alt=""></RouterLink>
             <RouterLink to="/subscription-plan"><img src="../../../assets/images/crown-icon.png" alt=""></RouterLink>
             <RouterLink to="/dashboard"><img src="../../../assets/images/usuario-de-perfil.png" alt=""></RouterLink>
+        </div>
+        <div class="menu">
+            <div class="hamburger-menu" @click="toggleMenu">
+    <div class="bar" :class="{ active: isOpen }"></div>
+    <div class="bar" :class="{ active: isOpen }"></div>
+    <div class="bar" :class="{ active: isOpen }"></div>
+    <div v-if="isOpen" class="dropdown">
+      <router-link to="/" @click="toggleMenu">Home</router-link>
+      <router-link to="/about" @click="toggleMenu">About</router-link>
+      <router-link to="/services" @click="toggleMenu">Services</router-link>
+      <router-link to="/contact" @click="toggleMenu">Contact</router-link>
+    </div>
+  </div>
         </div>
     </div>
 </template>
