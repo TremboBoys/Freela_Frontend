c0c3642a876a88b7312c34de06d72d92c3184431
dnzLucas <lucasdefenix@gmail.com>
2024-11-03 11:46:37 -0300

    CHORE: Add options in select categories #112

--- src/components/adsense/InputDesc.vue
+++ src/components/adsense/InputDesc.vue
@@ -1,15 +1,26 @@
 <script setup></script>
 
 <template>
-    <div class="publicoAlvo">
-        <label for="publicoAlvo">Publico Alvo</label>
-        <textarea type="text" id="publicoAlvo" placeholder="Ex: Pessoas que gostam de esportes" />
-    </div>
-    <div class="categories">
-        <select name="" id="">
-
-        </select>
-    </div>
+  <div class="publicoAlvo">
+    <label for="publicoAlvo">Publico Alvo</label>
+    <textarea type="text" id="publicoAlvo" placeholder="Ex: Pessoas que gostam de esportes" />
+  </div>
+  <div class="categories">
+    <label for="categories">Categorias</label>
+    <select name="" id="">
+      <option>Desenvolvimento Web</option>
+      <option>Design Gráfico</option>
+      <option>Marketing Digital</option>
+      <option>Escrita e Tradução</option>
+      <option>Suporte Administrativo</option>
+      <option>Finanças e Contabilidade</option>
+      <option>TI e Programação</option>
+      <option>Multimídia</option>
+      <option>Engenharia e Arquitetura</option>
+      <option>Consultoria</option>
+      <option>Outros</option>
+    </select>
+  </div>
 </template>
 
 <style>
