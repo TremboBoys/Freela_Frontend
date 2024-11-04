e09c6b7912cc9987d7cf25350fb5ca1637a1181a
dnzLucas <lucasdefenix@gmail.com>
2024-11-03 22:01:09 -0300

    CHORE: Add Hamburger menu for small screens in header component #132

--- src/components/default/header/RightSide.vue
+++ src/components/default/header/RightSide.vue
@@ -1,33 +1,24 @@
 <script setup>
-import { RouterLink } from 'vue-router';
 import { ref } from 'vue';
+import HeaderIcons from './HeaderIcons.vue';
 
-const isOpen = ref(false);
+import HamburgerButton from './HamburgerButton.vue';
+import ListSmallScreens from './ListSmallScreens.vue';
 
-const toggleMenu = () => {
-  isOpen.value = !isOpen.value;
-};
+const isActive = ref(false);
+
+function toggleMenu() {
+  isActive.value = !isActive.value;
+}
+
 </script>
 
 <template>
     <div class="header-rightSide">
         <div class="icons">
-            <RouterLink to="/chat"><img src="../../../assets/images/chat-icon.png" alt=""></RouterLink>
-            <RouterLink to="/subscription-plan"><img src="../../../assets/images/crown-icon.png" alt=""></RouterLink>
-            <RouterLink to="/dashboard"><img src="../../../assets/images/usuario-de-perfil.png" alt=""></RouterLink>
-        </div>
-        <div class="menu">
-            <div class="hamburger-menu" @click="toggleMenu">
-    <div class="bar" :class="{ active: isOpen }"></div>
-    <div class="bar" :class="{ active: isOpen }"></div>
-    <div class="bar" :class="{ active: isOpen }"></div>
-    <div v-if="isOpen" class="dropdown">
-      <router-link to="/" @click="toggleMenu">Home</router-link>
-      <router-link to="/about" @click="toggleMenu">About</router-link>
-      <router-link to="/services" @click="toggleMenu">Services</router-link>
-      <router-link to="/contact" @click="toggleMenu">Contact</router-link>
-    </div>
-  </div>
+            <HeaderIcons/>
+            <ListSmallScreens :isActive="isActive"/>
+            <HamburgerButton @toggle="toggleMenu" />
         </div>
     </div>
 </template>
