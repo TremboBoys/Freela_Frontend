919f7da463a4a8757b79447bb2ee8ab923deecee
dnzLucas <lucasdefenix@gmail.com>
2024-11-03 22:01:09 -0300

    CHORE: Add Hamburger menu for small screens in header component #132

---
+++ src/components/default/header/HamburgerButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  isActive: Boolean,
+});
+const emit = defineEmits(['toggle']);
+
+function toggleMenu() {
+  emit('toggle');
+}
+</script>
+
+<template>
+    <button :class="{ active: props.isActive }" class="hamburger" @click="toggleMenu"></button>
+</template>
+
+<style scoped>
+@import '../../../assets/Sass/default/header/_hambugerButton.scss';
+</style>
---
+++ src/components/default/header/HeaderIcons.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <div class="icons-container">
+        <div class="icons">
+            <RouterLink to="/chat"><img src="../../../assets/images/chat-icon.png" alt=""></RouterLink>
+            <RouterLink to="/subscription-plan"><img src="../../../assets/images/crown-icon.png" alt=""></RouterLink>
+            <RouterLink to="/dashboard"><img src="../../../assets/images/usuario-de-perfil.png" alt=""></RouterLink>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+@import '../../../assets/Sass/default/header/_rightSide.scss';
+</style>
