0839c0f21b14ec74629e2564daeb05ce5bb30eb1
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-11-04 22:07:19 -0300

    FEAT: Create initial configurations for support the chat #153

--- src/stores/chat/chat.js
+++ src/stores/chat/chat.js
@@ -1,26 +1,52 @@
 import { defineStore } from "pinia";
 import { reactive, ref } from "vue";
+import { io } from "socket.io-client";
 
 export const useChatStore = defineStore('chat', () => {
     const messages = ref([]);
+    const currentReceiver = ref(null);
+    const socket = io();
+
     const state = reactive({
         fileInput: null,
     });
-    const newMessage = reactive({
-        message: '',
-        file: null
-    });
-    function sendMessage() {
-        if (!newMessage.message && !newMessage.file) {
-            alert('Please enter a message or select a file');
-            return;
-        }
-        messages.value.push({
-            text: newMessage.message,
-            file: newMessage.file ? newMessage.file : null,
+    const newMessage = ref('');
+
+    function connectChat() {
+        socket.on("connect", () => {
+            console.log(socket.id);
         });
-        newMessage.message = '';
-        newMessage.file = null;
+
+        saveUser();
+
+    };
+
+    function
+
+    function saveUser() {
+        socket.emit("saveUser", 'usuarioPinia');
+    };
+    function sendMessage() {
+        socket.emit("sendMessage", ('usuarioPinia', currentReceiver, newMessage.value));
     }
-    return { messages, state, newMessage, sendMessage };
+    // socket.on("receivedMessage", (userSender, msg) => {
+    //     if (userSender === currentReceiver.value) {
+    //         messagesReceived.value = msg;
+    //     } else {
+    //         const users = Object.keys(otherMessages);
+    //         let userExist = false;
+    //         for (let user of users) {
+    //             if (user === userSender) {
+    //                 otherMessages.value[userSender].lastMessage = msg;
+    //                 otherMessages.value[userSender].numberMessages += 1;
+    //                 break;
+    //             }
+    //         }
+    //         if (!userExist) {
+    //             otherMessages.value[userSender].lastMessage = msg;
+    //             otherMessages.value[userSender].numberMessages = 1;
+    //         }
+    //     }
+    // })
+    return { messages, currentReceiver, state, newMessage, connectChat, saveUser, sendMessage };
 });
