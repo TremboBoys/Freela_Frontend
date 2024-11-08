24f6813a35d5decefb5e47f12fff952d73bef55a
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-11-08 10:59:42 -0300

    FEAT: add central section in help page #111

--- src/components/help/contact/contactPage.vue
+++ src/components/help/contact/contactPage.vue
@@ -1,28 +1,31 @@
 <script setup>
  import contactTitle from './contactTitle.vue';
  import questionsHelp from './questionsHelp.vue';
+ import helpCentral from './helpCentral.vue';
 </script>
 <template>
   <main>
     <contactTitle />
     <questionsHelp />
-    <div class="help-central">
-      <div class="help-title"><p>Central de ajuda</p></div>
-      <div class="instructions">
-        <div class="instruction">
-          <div class="number"><p>1)</p></div>
-          <div class="text"><p>Selecione seu perfil de consulta</p></div>
-        </div>
-        <div class="instruction">
-          <div class="number"><p>2)</p></div>
-          <div class="text"><p>Indique sua consulta no painel de ajuda</p></div>
-        </div>
-        <div class="instruction">
-          <div class="number"><p>3)</p></div>
-          <div class="text"><p>Solução disponível para sua consulta</p></div>
-        </div>
+    <helpCentral />
+    <div class="profile">
+      <div class="profile-title">
+      <div class="number">
+        <p>1</p>
+      </div>
+      <div class="text">
+        <p>Selecione seu perfil de consulta</p>
       </div>
     </div>
+    <div class="select-Perfil">
+        <button class="client">
+          <p>Sou Cliente</p>
+        </button>
+        <button class="freelancer">
+          <p>Sou Freelancer</p>
+        </button>
+     </div>
+  </div>
   </main>
 </template>
 <style scoped>
