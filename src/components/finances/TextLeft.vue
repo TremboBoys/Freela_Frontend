fd58b10f3bf94acb5ae79f6a21ecee8c9c6f14d1
LuizBlume <luiz.fernando.251107@gmail.com>
2024-11-18 13:44:13 -0300

    CHORE: Add new component to left finance page #202.

---
+++ src/components/finances/TextLeft.vue
@@ -0,0 +1,7 @@
+<template>
+    <div style="width: 40vw; padding: 20px;">
+        <h1>Introdução ao Gráfico de Renda Mensal</h1>
+        <p>Este gráfico mostra a renda mensal de todos os meses do ano.</p>
+        <canvas id="incomeChart"></canvas>
+    </div>
+</template>
--- src/views/finances/FinancesView.vue
+++ src/views/finances/FinancesView.vue
@@ -1,6 +1,17 @@
 <script setup>
 import GraphFinance from '@/components/finances/GraphFinance.vue';
+import TextLeft from '@/components/finances/TextLeft.vue';
 </script>
 <template>
-  <GraphFinance />
+  <div class="container">
+    <div class="left">
+      <TextLeft />
+    </div>
+    <div class="right">
+      <GraphFinance />
+    </div>
+  </div>
 </template>
+<style scoped>
+@import '@/assets/Sass/finances/_finances.scss';
+</style>
