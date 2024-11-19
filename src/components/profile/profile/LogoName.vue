8702aab4273ef18f7d2edcbb0e46939ae1bcb260
LuizBlume <luiz.fernando.251107@gmail.com>
2024-11-19 20:12:25 -0300

    FEAT: Profile page finish and remove components #197.

---
+++ src/components/profile/profile/LogoName.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps(['photo', 'name', 'area', 'nacionality']);
+</script>
+
+
+<template>
+    <div class="container">
+        <div class="info">
+            <div class="logo">
+                <img src="@/assets/images/dashboard/luiz.jpg" class="img-logo" alt="">
+            </div>
+            <div class="details">
+                <div class="name">
+                    <h1>{{ props.name }}</h1>
+                </div>
+                <div class="additional-info">
+                    <p class="description">{{ props.area }}</p>
+                    <div class="country">
+                        <img src="@/assets/images/dashboard/brasil-country.webp" alt="">
+                        <p>Brasil</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+@import '@/assets/Sass/profile/profile/_logoName.scss';
+</style>
---
+++ src/components/profile/projects/projectProgress/CardProject.vue
@@ -0,0 +1,49 @@
+<template>
+        <div class="card">
+            <div class="title">
+                <h2>Agente de IA</h2>
+            </div>
+            <div class="term">
+                <p>Prazo: 10/10/2024</p>
+            </div>
+            <div class="description">
+                <p>Seria uma empresa que vende mudanças residenciais e comerciais para todo o Brasil principalmente RJ e SP a pessoa teria acesso a um numero de whatsap e iria tentar fechar essa mudança com o cliente mosntrando quais são nossas vantagens e tentando superar a concorrencia. Com bonus de comissão por venda</p>
+            </div>
+            <div class="footer">
+                <div class="pending">
+                    <img src="@/assets/images/dashboard/clock-icon.png" class="clock" alt="">
+                    <span>Status: Pendente</span>
+                </div>
+                <div class="right">
+                    <div class="help">
+                        <img src="@/assets/images/dashboard/interrogacao.png" class="helpImg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="title">
+                <h2>Agente de IA</h2>
+            </div>
+            <div class="term">
+                <p>Prazo: 10/10/2024</p>
+            </div>
+            <div class="description">
+                <p>Seria uma empresa que vende mudanças residenciais e comerciais para todo o Brasil principalmente RJ e SP a pessoa teria acesso a um numero de whatsap e iria tentar fechar essa mudança com o cliente mosntrando quais são nossas vantagens e tentando superar a concorrencia. Com bonus de comissão por venda</p>
+            </div>
+            <div class="footer">
+                <div class="pending">
+                    <img src="@/assets/images/dashboard/clock-icon.png" class="clock" alt="">
+                    <span>Status: Pendente</span>
+                </div>
+                <div class="right">
+                    <div class="help">
+                        <img src="@/assets/images/dashboard/interrogacao.png" class="helpImg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+<style scoped>
+@import '@/assets/Sass/profile/projects/projectProgress/_cardProject.scss';
+</style>
