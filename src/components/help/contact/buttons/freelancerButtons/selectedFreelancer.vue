9b36bedfba303367fa0679597e0aeda6190ac6eb
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-11-27 08:42:48 -0300

    FEAT: component buttons styles #111

--- src/components/help/contact/buttons/freelancerButtons/selectedFreelancer.vue
+++ src/components/help/contact/buttons/freelancerButtons/selectedFreelancer.vue
@@ -5,31 +5,5 @@
         </button>
 </template>
 <style scoped lang="scss">
-button{
-    background-color: #F2F2F2;
-    border: 2px solid #0b5ebd;
-    border-radius: 12px;
-    width: 18vw;
-    height: 7vh;
-    margin-top: 10px;
-    margin-bottom: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    p{
-        font-size: 1.2rem;
-        font-weight: 500;   
-    }
-    transition: background-color 0.3s ease, transform 0.4s ease;
-}
-button:hover {
-    background-color: #D9D9D9;
-    transform: scale(1.1);
-    transition: background-color 0.3s ease, transform 0.4s ease;
-}
-button:active {
-  background-color: #BFBFBF;
-  transform: scale(1);
-  transition: background-color 0.3s ease, transform 0.4s ease;
-}
+@import "@/assets/Sass/help/contact/buttons/selectedButton.scss";
 </style>
