2be1ef2ac809ddede7a6a4288b4f5c37ba85dbde
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-11-27 17:10:10 -0300

    STYLE: Continue styling pop-up-mp

--- src/components/finances/PopUpMp.vue
+++ src/components/finances/PopUpMp.vue
@@ -10,6 +10,13 @@
         </div>
         <div class="container-request">
             <h2>Conecte-se ao Mercado Pago para receber seus pagamentos.</h2>
+            <div class="container-button-authorization">
+                <p class="description">Para conseguirmos direcionar os pagamentos a sua conta, precisamos que você autorize as transações.</p>
+
+                <button><a href="https://auth.mercadopago.com.br/authorization?response_type=code&client_id=APP_USR-2559043759198862-100917-d4ac3989d9faf53aa141c3649496d98d-199834452&redirect_uri=https://ms-pix.onrender.com/authClient/">Autorizar nosso sistema</a></button>
+
+                <p class="observation">Caso ainda não tenha uma conta no Mercado Pago, crie uma antes com o mesmo email de sua conta no Tasker.</p>
+            </div>
         </div>
     </div>
 </template>
