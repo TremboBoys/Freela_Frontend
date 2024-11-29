41f83429900c7ddfc34a2cd1cb6b4752366a3fc2
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-11-28 23:41:46 -0300

    FEAT: links #111

--- src/components/help/contact/contactPage.vue
+++ src/components/help/contact/contactPage.vue
@@ -6,12 +6,26 @@
 </script>
 <template>
   <main>
-    <contactTitle />
-    <questionsHelp />
-    <helpCentral />
-    <profileSections />
+    <div class="help-questions">
+      <contactTitle />
+      <questionsHelp />
+      <helpCentral />
+      <profileSections />
+    </div>
+    <div class="links">
+      <h1>Links</h1>
+    </div>
   </main>
 </template>
 <style scoped>
   @import '@/assets/Sass/help/contact/_contactPage.scss';
+ .links {
+    background-color: yellow;
+    width: 20%;
+    border: 1px solid black;
+    margin-right: 5%;
+    margin-bottom: 10vh;
+    position: absolute;
+    right: 0;
+  }
 </style>
